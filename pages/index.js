dc1faa6a44ebaafb9f2863684d705b8bf65d66a3
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-12 21:27:32 +0530

    Getting Data From The Database

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
 import MeetupList from '../components/meetups/MeetupList'
 import Layout from '../components/layout/Layout'
 import { useEffect } from 'react';
+import { MongoClient } from 'mongodb';
 
-const DUMMY_MEETUPS = [
-    {
-      id: 'm1',
-      title: 'A First Meetup',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Some address 5, 12345 Some City',
-      description: 'This is a first meetup!'
-    },
-    {
-      id: 'm2',
-      title: 'A Second Meetup',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Some address 10, 12345 Some City',
-      description: 'This is a second meetup!'
-    }
-  ];
+// const DUMMY_MEETUPS = [
+//     {
+//       id: 'm1',
+//       title: 'A First Meetup',
+//       image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+//       address: 'Some address 5, 12345 Some City',
+//       description: 'This is a first meetup!'
+//     },
+//     {
+//       id: 'm2',
+//       title: 'A Second Meetup',
+//       image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+//       address: 'Some address 10, 12345 Some City',
+//       description: 'This is a second meetup!'
+//     }
+//   ];  
   
-
 const HomePage = (props) => {
   // const [loadedMeetups,setLoadedMeetups]=useState([])
   // useEffect(()=>{
@@ -31,25 +31,35 @@ const HomePage = (props) => {
   )
 }
 
-// export async function getStaticProps(){
-// // fetch data from an API
-// return{
-//   props:{
-//     meetups:DUMMY_MEETUPS
-//   },
-//   revalidate:10
-// }
-// }
-
-export async function getServerSideProps(context){
-  const req=context.req;
-  const res=context.res;
-  
-  return{
-    props:{
-      meetups:DUMMY_MEETUPS
-    }
-  }
+export async function getStaticProps(){
+// fetch data from an API
+const client = await MongoClient.connect('mongodb+srv://sunset008:zhz5dnogOPN6jSry@cluster0.taegvij.mongodb.net/meetup?retryWrites=true&w=majority')
+const db=client.db()
+const meetupsCollection= db.collection('meetups-collection');
+const meetups= await meetupsCollection.find().toArray()
+client.close()
+return{
+  props:{
+    meetups:meetups.map(meetup=>({
+      title:meetup.title,
+      address:meetup.address,
+      image:meetup.image,
+      id:meetup._id.toString(),
+    }))
+  },
+  revalidate:10
 }
+}
+
+// export async function getServerSideProps(context){
+//   const req=context.req;
+//   const res=context.res;
+  
+//   return{
+//     props:{
+//       meetups:DUMMY_MEETUPS
+//     }
+//   }
+// }
 
 export default HomePage
