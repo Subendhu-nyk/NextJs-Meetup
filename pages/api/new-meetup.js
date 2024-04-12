d19f54c901b32503213dca2067fa629f223edafb
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-04-12 20:38:10 +0530

    mongodb api route

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,17 @@
+import { MongoClient } from "mongodb";
 
-const handler=(req,res)=>{
+
+const handler=async(req,res)=>{
     if (req.method==='POST'){
         const data=req.body;
-        const {title,image,address,description}=data
+        // const {title,image,address,description}=data
+       const client = await MongoClient.connect('mongodb+srv://sunset008:zhz5dnogOPN6jSry@cluster0.taegvij.mongodb.net/meetup?retryWrites=true&w=majority')
+       const db=client.db()
+       const meetupsCollection= db.collection('meetups-collection');
+       const result= await meetupsCollection.insertOne(data)
+       console.log(result);
+       client.close()
+       res.status(201).json({message:"meetup inserted!"})
     }
 }
 export default handler;
